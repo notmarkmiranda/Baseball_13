28008bb6a7a6c27696d2b26c6a4e760d3a0fc246
mark miranda <notmarkmiranda@gmail.com>
2018-03-30 15:28:58 -0600

    Refactor styling and database calls

--- app/models/accomplishment.rb
+++ app/models/accomplishment.rb
@@ -5,6 +5,10 @@ class Accomplishment < ApplicationRecord
     find_by(team_id: team.id, number: number).nil? ? '' : 'X'
   end
 
+  def self.team_accomplishment_exists?(team, number)
+    find_by(team_id: team.id, number: number)
+  end
+
   def self.count_by_team(team)
     where("team_id = ? AND number != ?", team.id, 99).count
   end
